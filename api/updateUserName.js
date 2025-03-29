561b9cdd01afa836c905d904608a5c301f868c31
Jaivardhan Bhola <jaivardhan.bhola@gmail.com>
2025-03-29 16:02:41 +0530

    bug fixes

--- api/updateUserName.js
+++ api/updateUserName.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-router.put('/', async (req, res) => {
+router.post('/', async (req, res) => {
     try {
         const { name, email } = req.body;
-        
+
         if (!name || !email) {
             return res.status(400).json({
                 success: false,
